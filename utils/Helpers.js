91c167d3d6875a676d0826ea6b200839e2939790
painor <30215770+painor@users.noreply.github.com>
2019-09-11 22:36:22 +0100

    Add getRandomInt to helpers.

--- utils/Helpers.js
+++ utils/Helpers.js
@@ -112,6 +112,19 @@ class Helpers {
         return shaSum.digest();
 
     }
+
+    /**
+     * returns a random int from min (inclusive) and max (inclusive)
+     * @param min
+     * @param max
+     * @returns {number}
+     */
+    static getRandomInt(min, max) {
+        min = Math.ceil(min);
+        max = Math.floor(max);
+        return Math.floor(Math.random() * (max - min + 1)) + min;
+    }
+
 }
 
 exports.helpers = Helpers;
