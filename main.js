0b84f7dbab6eab30787b3baf4c44f2592a1722ca
painor <Ireallydontwannashareit@example.com>
2019-09-28 17:28:27 +0100

    Add support for BigInt Fix AuthKey Remove Debug logs Start working on interactive example

---
+++ main.js
@@ -0,0 +1,10 @@
+const {Helpers} = require("./utils/Helpers");
+const {TelegramClient} = require("./tl/TelegramClient");
+
+(async function () {
+    console.log("Loading interactive example...");
+    let settings = Helpers.loadSettings();
+    let client = TelegramClient(settings["session_name"], 55, settings["api_id"], settings["api_hash"]);
+    await client.connect();
+    console.log("You should now be connected.");
+})();
