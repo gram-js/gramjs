63685cb5bc1038f9a92414f0659f64905510a815
painor <pi.oussama@gmail.com>
2020-12-02 15:11:25 +0100

    Update example

--- examples/main.js
+++ examples/main.js
@@ -1,13 +1,13 @@
-const { TelegramClient } = require('../gramjs')
+const { TelegramClient } = require('../gramjs');
+const { StringSession } = require('../gramjs').sessions;
 
 
 (async () => {
     console.log('Loading interactive example...')
     const sessionName = 'anon'
-    const apiId = -1
-    const apiHash = ''
-    const client = new TelegramClient(sessionName, apiId, apiHash)
+    const apiId = -1 // put your api id here [for example 123456789]
+    const apiHash = "" // put your api hash here [for example '123456abcfghe']
+    const client = new TelegramClient(new StringSession(''), apiId, apiHash)
     await client.connect()
-
     console.log('You should now be connected.')
 })()
