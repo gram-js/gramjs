39afe55335f947af42993446cfff3cb7cc370f9d
Coxxs <3007303+Coxxs@users.noreply.github.com>
2022-07-26 04:58:42 +0900

    Add proper support for `flags2` (#366)
    * regenerate api.d.ts
    
    * Format code with Prettier
    
    Co-authored-by: Coxxs <coxxs@users.noreply.github.com>

--- gramjs/tl/api.js
+++ gramjs/tl/api.js
@@ -315,18 +315,18 @@ function createClasses(classesType, params) {
                         if (arg.isFlag) {
                             if (arg.type === "true") {
                                 args[argName] = Boolean(
-                                    args["flags"] & (1 << arg.flagIndex)
+                                    args[arg.flagName] & (1 << arg.flagIndex)
                                 );
                                 continue;
                             }
-                            if (args["flags"] & (1 << arg.flagIndex)) {
+                            if (args[arg.flagName] & (1 << arg.flagIndex)) {
                                 args[argName] = getArgFromReader(reader, arg);
                             } else {
                                 args[argName] = null;
                             }
                         } else {
                             if (arg.flagIndicator) {
-                                arg.name = "flags";
+                                arg.name = argName;
                             }
                             args[argName] = getArgFromReader(reader, arg);
                         }
@@ -338,7 +338,10 @@ function createClasses(classesType, params) {
             validate() {
                 for (const arg in argsConfig) {
                     if (argsConfig.hasOwnProperty(arg)) {
-                        if (arg === "flags" || argsConfig[arg].isFlag) {
+                        if (
+                            argsConfig[arg].flagIndicator ||
+                            argsConfig[arg].isFlag
+                        ) {
                             // we don't care about flags
                             continue;
                         }
@@ -458,7 +461,10 @@ function createClasses(classesType, params) {
                             } else {
                                 let flagCalculate = 0;
                                 for (const f in argsConfig) {
-                                    if (argsConfig[f].isFlag) {
+                                    if (
+                                        argsConfig[f].isFlag &&
+                                        arg === argsConfig[f].flagName
+                                    ) {
                                         if (
                                             (this[f] === false && argsConfig[f].type !== 'Bool') ||
                                             this[f] === undefined ||
--- gramjs/tl/types-generator/template.js
+++ gramjs/tl/types-generator/template.js
@@ -78,7 +78,8 @@ ${indent}static fromReader(reader: Reader): ${upperFirst(name)};
 
                 const hasRequiredArgs = argKeys.some(
                     (argName) =>
-                        argName !== "flags" && !argsConfig[argName].isFlag
+                        !argsConfig[argName].flagIndicator &&
+                        !argsConfig[argName].isFlag
                 );
 
                 return `
@@ -135,7 +136,8 @@ ${indent}static fromReader(reader: Reader): ${upperFirst(name)};
 
                 const hasRequiredArgs = argKeys.some(
                     (argName) =>
-                        argName !== "flags" && !argsConfig[argName].isFlag
+                        !argsConfig[argName].flagIndicator &&
+                        !argsConfig[argName].isFlag
                 );
 
                 return `
@@ -177,10 +179,10 @@ ${indent}}`.trim();
     }
 
     function renderArg(argName, argConfig) {
-        const { isVector, isFlag, skipConstructorId, type } = argConfig;
+        const { isVector, isFlag, skipConstructorId, flagIndicator, type } = argConfig;
 
         const valueType = renderValueType(type, isVector, !skipConstructorId);
-        return `${argName === "flags" ? "// " : ""}${argName}${
+        return `${flagIndicator ? "// " : ""}${argName}${
             isFlag || (argName === "randomId" && type === "long" && !isVector)
                 ? "?"
                 : ""
