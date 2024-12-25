7307347017990881661a69752e3da74bd4b27769
Vano Devium <webdevium@gmail.com>
2024-12-25 16:54:49 +0200

    Export events like you export another things

--- gramjs/events/index.ts
+++ gramjs/events/index.ts
@@ -1,2 +1,6 @@
 export { Raw } from "./Raw";
 export { NewMessage, NewMessageEvent } from "./NewMessage";
+export { EditedMessage, EditedMessageEvent } from "./EditedMessage";
+export { DeletedMessage, DeletedMessageEvent } from "./DeletedMessage";
+export { CallbackQuery, CallbackQueryEvent } from "./CallbackQuery";
+export { Album, AlbumEvent } from "./Album";
--- gramjs/index.ts
+++ gramjs/index.ts
@@ -1,15 +1,27 @@
 export { Api } from "./tl";
 import * as tl from "./tl";
+
 export { TelegramClient } from "./client/TelegramClient";
 export { Connection } from "./network";
 export { version } from "./Version";
 export { Logger } from "./extensions/Logger";
 import * as utils from "./Utils";
 import * as errors from "./errors";
+import * as events from "./events";
 import * as sessions from "./sessions";
 import * as extensions from "./extensions";
 import * as helpers from "./Helpers";
 import * as client from "./client";
 import * as password from "./Password";
 
-export { utils, errors, sessions, extensions, helpers, tl, password, client };
+export {
+    utils,
+    errors,
+    events,
+    sessions,
+    extensions,
+    helpers,
+    tl,
+    password,
+    client,
+};
